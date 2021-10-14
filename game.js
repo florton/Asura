19a86be590bad61a04d2a2e7d4bd8444e3c804bf
Flanders Lorton <flanders.lorton@nelson.com>
2021-10-14 00:08:53 -0600

    updates

--- game.js
+++ game.js
@@ -4,19 +4,19 @@ const assets = {
       id: 0,
       name: "Dagger",
       damage: 3,
-      moves: [2]
+      moves: [1, 2, 8]
     },
     1: {
       id: 1,
       name: "Heavy Club",
       damage: 8,
-      moves: [0]
+      moves: [1, 6, 9]
     },
     2: {
       id: 2,
       name: "Scimitar",
       damage: 7,
-      moves: [1]
+      moves: [1, 7, 8]
     },
     2: {
       id: 2,
@@ -25,39 +25,55 @@ const assets = {
       moves: [2, 3, 4]
     },
     3: {
-      id: 0,
+      id: 3,
       name: "Katana",
       damage: 5,
-      moves: [1, 2]
+      moves: [1, 7, 2]
+    },
+    4: {
+      id: 4,
+      name: "Ivory Wand",
+      damage: 7,
+      moves: [2, 3, 5]
     },
   },
   enemies: {
     0: {
       id: 0,
       name: "Skeleton",
-      hp: 20,
+      hp: 30,
+      damage: 3,
       moves: [0, 1, 2, 5]
     },
-    0: {
-      id: 0,
+    1: {
+      id: 1,
       name: "Demon",
-      hp: 20,
+      hp: 40,
+      damage: 5,
       moves: [3, 4, 5, 6]
+    },
+    2: {
+      id: 1,
+      name: "Witch",
+      hp: 35,
+      damage: 3,
+      moves: [4, 7, 8]
+    },
+    3: {
+      id: 3,
+      name: "Minor God",
+      hp: 60,
+      damage: 7,
+      moves: [1, 2, 6, 9]
     }
   },
   moves: {
     player : {
-      0: {
-        id: 0,
-        name: "Crush",
-        damage: 8,
-        accuracy: 0.75,
-      },
       1: {
         id: 1,
-        name: "Slice",
-        damage: 6,
-        accuracy: 0.9,
+        name: "Block",
+        accuracy: 1,
+        special: {type: 'block', value: 10}
       },
       2: {
         id: 2,
@@ -78,8 +94,39 @@ const assets = {
         damage: 9,
         accuracy: 0.75,
       },
+      5: {
+        id: 5,
+        name: "Lesser Banishing",
+        damage: 2,
+        accuracy: 0.75,
+        special: {type: "debuff-damage", value: 0.8}
+      },
+      6: {
+        id: 6,
+        name: "Crush",
+        damage: 8,
+        accuracy: 0.75,
+      },
+      7: {
+        id: 7,
+        name: "Slice",
+        damage: 6,
+        accuracy: 0.9,
+      },
+      8: {
+        id: 8,
+        name: "Slash",
+        damage: 6,
+        accuracy: 0.75,
+      },
+      9: {
+        id: 9,
+        name: "Swing",
+        damage: 6,
+        accuracy: 1,
+      },
     },
-    ememy : {
+    enemy : {
       0: {
         id: 0,
         name: "Bone Roller",
@@ -104,13 +151,13 @@ const assets = {
         name: "Dark Oath",
         damage: 0,
         accuracy: 1,
-        special: {type: "boost-damage", value: 1.5}
+        special: {type: "buff-damage", value: 1.5}
       },
       4: {
         id: 4,
         name: "Evil Eye",
         damage: 4,
-        accuracy: 1,
+        accuracy: 0.95,
       },
       5: {
         id: 5,
@@ -123,6 +170,25 @@ const assets = {
         name: "Dark Power",
         damage: 8,
         accuracy: 0.7,
+      },
+      7: {
+        id: 7,
+        name: "Weaken",
+        damage: 0,
+        accuracy: 1,
+        special: {type: "debuff-damage", value: 0.9}
+      },
+      8: {
+        id: 8,
+        name: "Cackle",
+        damage: 2,
+        accuracy: 1,
+      },
+      9: {
+        id: 9,
+        name: "Cosmic Eye",
+        damage: 22,
+        accuracy: 0.1,
       }
     }
   },
@@ -143,6 +209,22 @@ const assets = {
       id: 3,
       name: "a Burning Desert"
     },
+    4: {
+      id: 3,
+      name: "an Old Schoolhouse"
+    },
+    5: {
+      id: 5,
+      name: "a Dark Cavern"
+    },
+    6: {
+      id: 6,
+      name: "a Dense Forest"
+    },
+    7: {
+      id: 7,
+      name: "a Ruined Temple"
+    },
   }
 }
 
@@ -155,10 +237,11 @@ prompt.message = ''
 prompt.colors = false;
 
 const random = (array) => array[Math.floor((Math.random()*array.length))]
+const rand = () => Math.random()
 
 const clone = (obj) => JSON.parse(JSON.stringify(obj))
 
-const randomInt = (min, max) => Math.random() * (max - min) + min
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
 const randomAsset = (asset) => clone(random(Object.values(asset)))
 
@@ -171,7 +254,7 @@ const lineBreak = () => {
 // ---------------------------------------------
 
 let player = {}
-let gamestate = {}
+let encounter = {}
 
 const start = () => {
   player = {
@@ -179,19 +262,26 @@ const start = () => {
     gold: 0,
     weapons: [0],
     equip: {
-      weapon: [0]
+      weapon: 0
+    },
+    buffs: { 
+      attack: 1,
+      block: 0
     }
   }
 
-  gamestate = {
-    encounter : {
-      index: 0
-    }
+   encounter = {
+    index: 0
+    // enemy,
+    // loot: {
+    //   item: 1,
+    //   gold: 100
+    // }
   }
 
+  lineBreak()
   console.log("Welcome to Asura!")
   console.log("we hope you enjoy your stay")
-  lineBreak()
 
   main()
 }
@@ -204,27 +294,47 @@ const newEncounter = () => {
 
   const scene = randomAsset(assets.scenes)
 
-  gamestate.encounter = {
-    index: gamestate.encounter.index + 1,
-    enemy,
+  encounter = {
+    index: encounter.index + 1,
+    enemy : {
+      ...enemy,
+      buffs: { 
+        attack: 1,
+        block: 0
+      }
+    },
     loot: {
-      item,
+      item: item,
       gold
     }
   }
 
+  player = {
+    ...player,
+    buffs: { 
+      attack: 1,
+      block: 0
+    }
+  }
+
+  lineBreak()
+  lineBreak()
   console.log ("You find yourself in " + scene.name)
   console.log("In front of you you see a " + enemy.name)
+  console.log()
+  console.log("You have " + player.hp + " hp and " + player.gold + " gold")
 }
 
 const usePlayerMenu = async () => {
   const options = ["1", "2"]
   console.log("")
+  console.log("What do you want to do? ")
+  console.log("")
   console.log("1. Equip")
   console.log("2. Ability")
-  console.log("What do you want to do? ")
   const {choice} = await prompt.get(['choice'])
   if (!options.includes(choice)){
+    console.log('')
     console.log('Invaid choice')
     return usePlayerMenu()
   } else {
@@ -240,13 +350,14 @@ const useEquipmentMenu = async () => {
   console.log("Would you like to switch weapons?")
   console.log("")
   console.log("0. Cancel")
-  player.weapons.filter(x => x != currentWeapon.id).forEach((id, index) => {
+  player.weapons.forEach((id, index) => {
     const weapon = assets.weapons[id]
     options.push((index + 1) + "")
     console.log((index + 1) + ". " + weapon.name)
   })
   const {choice} = await prompt.get(['choice'])
   if (!options.includes(choice)){
+    console.log('')
     console.log('Invaid choice')
     return useEquipmentMenu()
   } else {
@@ -258,13 +369,14 @@ const useAbilityMenu = async () => {
   const currentWeapon = assets.weapons[player.equip.weapon]
   const options = ["0"]
   console.log("")
-  console.log("0. Block")
+  console.log("0. Cancel")
   Object.values(assets.moves.player).filter(x => currentWeapon.moves.includes(x.id)).forEach((move, index) => {
     options.push((index + 1) + "")
     console.log((index + 1) + ". " + move.name)
   })
   const {choice} = await prompt.get(['choice'])
   if (!options.includes(choice)){
+    console.log('')
     console.log('Invaid choice')
     return useAbilityMenu()
   } else {
@@ -272,39 +384,129 @@ const useAbilityMenu = async () => {
   }
 }
 
+const calculateDamage = (attacker, defender, move, isPlayer = false) => {
+  console.log("")
+  const youIt = isPlayer ? "You" : "The enemy"
+  const maybeS = isPlayer ? "" : "s"
+  if (rand() < move.accuracy) {
+    if (move.damage > 0) {
+      const attackerDamage = attacker.damage || assets.weapons[attacker.equip.weapon].damage
+      const damage = (
+        Math.floor(
+          (attackerDamage + move.damage) 
+          * attacker.buffs.attack 
+          * ((rand() / 2) + 1)
+        ) - defender.buffs.block
+      ) 
+      defender.hp -= damage
+      console.log("It does " + damage + " damage!")
+    }
+    if (move.special){
+      const { type, value } = move.special
+      if(type === "block"){
+        // only player can block
+        attacker.buffs.block = value
+        console.log(youIt + " get" + maybeS + " ready to block the enemy strike")
+      }else if (type === "heal"){
+        attacker.hp += value
+        console.log(youIt + " heal" + maybeS + " " + value + " hp")
+      }else if (type.startsWith("buff") || type.startsWith("debuff")){
+        const isBuff = type.startsWith("buff")
+        if (type.endsWith("damage")){
+          if (isBuff){
+            attacker.buffs.attack *= value
+            console.log(youIt + " feel" + maybeS + " stronger")
+          } else {
+            defender.buffs.attack *= value
+            console.log(youIt + " feel" + maybeS + " weaker")
+          }
+        }
+      }
+    }
+  } else {
+    console.log("But it missed!")
+  }
+}
+
 const playerTurn = async () => {
+  player.buffs.block = 0
+
   let turnIsOver = false
   while ( !turnIsOver ){
     let choice = await usePlayerMenu()
     if (choice == 1){
       choice = await useEquipmentMenu()
       if (choice != 0){
-        console.log("Change weapon to " + choice)
+        const newWeapon = assets.weapons[player.weapons[choice - 1]] 
+        player.equip.weapon = newWeapon.id
+        console.log("")
+        console.log("Weapon changed to " + newWeapon.name)
       }
     } else if (choice == 2){
       choice = await useAbilityMenu()
-      console.log("Use ability " + choice)
+      if (choice == 0){
+        continue
+      }
+      lineBreak()
+      const playerWeapon = assets.weapons[player.equip.weapon]
+      const chosenAbility = assets.moves.player[playerWeapon.moves[choice - 1]]
+      console.log("You use " + chosenAbility.name)
+      calculateDamage(player, encounter.enemy, chosenAbility, true)
+      if (chosenAbility.damage > 0){
+        console.log("")
+        const enemyHp = encounter.enemy.hp > 0 ? encounter.enemy.hp : 0
+        console.log("The " + encounter.enemy.name + " has " + enemyHp + " hp")
+      }
       turnIsOver = true
     }
   }
 }
 
 const enemyTurn = () => {
-  console.log("Ememy turn")
+  const enemyMoveId = random(encounter.enemy.moves)
+  const enemyMove = assets.moves.enemy[enemyMoveId]
+  lineBreak()
+  console.log(encounter.enemy.name + " uses " + enemyMove.name)
+  calculateDamage(encounter.enemy, player, enemyMove)
+  if (enemyMove.damage > 0){
+    console.log("")
+    const playerHp = player.hp > 0 ? player.hp : 0 
+    console.log("You have " +  playerHp + " hp")
+  }
+}
+
+const getLoot = () => {
+  player.gold += encounter.loot.gold
+  console.log("You find " + encounter.loot.gold + " gold")
+  player.weapons.push(encounter.loot.item.id)
+  console.log("You found a " + encounter.loot.item.name + "!")
+
 }
 
 const main = async () => {
   newEncounter ()
+  lineBreak()
 
-  while (gamestate.encounter.enemy.hp > 0){
+  while (encounter.enemy.hp > 0){
     if (player.hp < 0) {
       console.log ("YOU DIED")
       lineBreak()
       start()
+    } else {
+      await playerTurn()
+    
+      if (encounter.enemy.hp > 0) {
+        enemyTurn()
+      }
+      lineBreak()
     }
-    await playerTurn()
-    enemyTurn()
   }
+
+  console.log("You beat the " + encounter.enemy.name + "!")
+  console.log("")
+  getLoot()
+
+  main()
 }
 
 start ()
